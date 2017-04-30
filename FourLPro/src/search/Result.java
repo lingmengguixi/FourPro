06222b91afaebe42d970e3e9462ad8cbd945229b
李兆坤 <1600079541@qq.com>
2017-04-30 12:07:36 +0800

    changelzk

---
+++ FourLPro/src/search/Result.java
@@ -0,0 +1,15 @@
+package search;
+
+import java.util.ArrayList;
+
+public class Result {
+   private ArrayList<String> element=new ArrayList<String>();
+
+public ArrayList<String> getElement() {
+	return element;
+}
+
+public void setElement(ArrayList<String> element) {
+	this.element = element; 
+}
+}
---
+++ FourLPro/src/search/SetLimit.java
@@ -0,0 +1,12 @@
+package search;
+
+public class SetLimit {
+	 public boolean name=true;
+	 public boolean style=true;
+	 public boolean price=false;
+	 public boolean margin=true;
+	 public boolean id=false;
+	 public boolean hasMargin=true;
+	 public double startPrice=0;
+	 public double endPrice=1e20;
+} 
